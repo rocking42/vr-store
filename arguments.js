85f27537dacfdb188a2b95c1a930958b81a28852
rocking42 <nedpike3@gmail.com>
2016-12-16 13:15:34 +1100

    Added elephant

--- arguments.js
+++ arguments.js
@@ -1,3 +1,26 @@
-process.argv.forEach(function(val, index) {
-  console.log(index + ':' + val );
-});
+// process.argv.forEach(function(val, index) {
+//   console.log(index + ':' + val );
+// });
+// console.log('start');
+// // doesn't execute on the first loop but does on the next loop round
+// process.nextTick(function() {
+//   console.log('nextTick callback');
+// });
+// console.log('schechuled');
+var globalFoo;
+var main = [1,2,3,4];
+var next = { mine: 42};
+exports.setFoo = function(val) {
+  globalFoo = val;
+};
+
+exports.returnFoo = function() {
+  return globalFoo;
+};
+
+exports.includeFoo = function() {
+  return next;
+};
+
+exports.main = next;
+exports.thow = main;
