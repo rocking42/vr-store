d4d5a7bce15e0e7f3036d5cf57b6dc2ceada32b2
rocking42 <nedpike3@gmail.com>
2016-12-19 11:13:24 +1100

    Added trees

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
+
+// //
+// function returnMovieName(data) {
 //
-function returnMovieName(data) {
-
-    console.log($("#box").attr("textwrap").text);
-}
-
-
-
-
-function getMovieName(query) {
-    $.ajax({
-        url: 'http://omdbapi.com',
-        type: "GET",
-        dataType: "JSON",
-        data: {
-          t: query
-        }
-    }).done(returnMovieName);
-}
-
-
-AFRAME.registerComponent('scale-on-click', {
-  schema: {
-    to: {default: 'textAlign: center; x: 128; y: 128; text: Hello everyone'}
-  },
-  init: function () {
-    var data = this.data;
-    this.el.addEventListener('click', function () {
-      this.setAttribute('textwrap', data.to);
-    });
-  }
-});
+//     console.log($("#box").attr("textwrap").text);
+// }
+//
+//
+//
+//
+// function getMovieName(query) {
+//     $.ajax({
+//         url: 'http://omdbapi.com',
+//         type: "GET",
+//         dataType: "JSON",
+//         data: {
+//           t: query
+//         }
+//     }).done(returnMovieName);
+// }
+//
+//
+// AFRAME.registerComponent('scale-on-click', {
+//   schema: {
+//     to: {default: 'textAlign: center; x: 128; y: 128; text: Hello everyone'}
+//   },
+//   init: function () {
+//     var data = this.data;
+//     this.el.addEventListener('click', function () {
+//       this.setAttribute('textwrap', data.to);
+//     });
+//   }
+// });
 
 
 $(document).ready(function() {
